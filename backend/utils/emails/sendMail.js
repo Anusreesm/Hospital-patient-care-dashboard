3fe136da51650a8840aa2cfa6b4b551f2dfb1604
ANUSREE S M <anusm43cal@gmail.com>
2025-12-02 18:02:28 +0530

    mail issue

--- backend/utils/emails/sendMail.js
+++ backend/utils/emails/sendMail.js
@@ -10,18 +10,35 @@ const SendMail = async (toEmail, { subject, text, html }) => {
     try {
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
-            port: 587,
+            // port: 587,
+            port: 465,
+            secure: true, // SSL
             auth: {
                 user: process.env.USER_EMAIL,
                 pass: process.env.APP_PW
-            }
+            },
+            tls: {
+                rejectUnauthorized: false, // bypass cert issues on Render
+            },
         })
-        const info = await transporter.sendMail({
+        // Mail options
+        const mailOptions = {
             from: `"MedTech" <${process.env.USER_EMAIL}>`,
             to: toEmail,
             subject,
             text,
-            html
+            html,
+        };
+
+        // Send mail and await response
+        const info = await new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(info);
+                }
+            });
         });
         console.log(MESSAGES.COMMON.EMAIL_SEND, info.messageId);
     } catch (error) {
