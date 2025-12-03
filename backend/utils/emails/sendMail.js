053b162c4bff94934c830348e2a780eb4bc7a173
ANUSREE S M <anusm43cal@gmail.com>
2025-12-03 20:08:50 +0530

    final commit

--- backend/utils/emails/sendMail.js
+++ backend/utils/emails/sendMail.js
@@ -1,4 +1,4 @@
-import sgMail from "@sendgrid/mail"; 
+import nodemailer from 'nodemailer'
 import dotenv from "dotenv";
 
 import { MESSAGES } from "../../constants/messages.js";
@@ -8,17 +8,26 @@ dotenv.config({ path: './.env' })
 
 const SendMail = async (toEmail, { subject, text, html }) => {
     try {
-        
-        console.log("FROM_EMAIL:", process.env.FROM_EMAIL);
-        console.log("SENDGRID_API_KEY exists:", !!process.env.SENDGRID_API_KEY);
-       
 
+        console.log("USER_EMAIL:", process.env.USER_EMAIL);
+        console.log("APP_PW exists:", !!process.env.APP_PW);
+        console.log("APP_PW length:", process.env.APP_PW?.length); // Should be 16
 
-       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // STARTTLS
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.APP_PW
+            },
+            tls: {
+                rejectUnauthorized: false, // bypass cert issues on Render
+            },
+        });
         // Mail options
-        const message = {
-            from: `"MedTech" <${process.env.FROM_EMAIL}>`,
+        const mailOptions = {
+            from: `"MedTech" <${process.env.USER_EMAIL}>`,
             to: toEmail,
             subject,
             text,
@@ -26,17 +35,15 @@ const SendMail = async (toEmail, { subject, text, html }) => {
         };
 
         // Send mail and await response
-  const response = await sgMail.send(message);
+     const info = await transporter.sendMail(mailOptions);
 
-    console.log("Email sent...");
-    console.log("Status:", response[0].statusCode);
-
-    return response;
-            
+    console.log("Email sent successfully:", info.messageId);
+    return info; // Return success info
     } catch (error) {
         console.error(MESSAGES.COMMON.EMAIL_ERROR, error);
         throw error;
     }
 };
+
 
 export default SendMail;
