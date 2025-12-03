6fa6a41daae9082ba471981526416bcd4fe3493d
ANUSREE S M <anusm43cal@gmail.com>
2025-12-04 00:48:57 +0530

    send mail correct

--- backend/utils/emails/sendMail.js
+++ backend/utils/emails/sendMail.js
@@ -3,45 +3,32 @@ import dotenv from "dotenv";
 
 import { MESSAGES } from "../../constants/messages.js";
 
+dotenv.config()
 
-dotenv.config({ path: './.env' })
+const transporter=nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:587,
+    secure: false,
+    auth:{
+        user:process.env.USER_EMAIL,
+        pass:process.env.USER_PASSWORD,
+    }
+    })
 
 const SendMail = async (toEmail, { subject, text, html }) => {
     try {
-
-        console.log("USER_EMAIL:", process.env.USER_EMAIL);
-        console.log("APP_PW exists:", !!process.env.APP_PW);
-        console.log("APP_PW length:", process.env.APP_PW?.length); // Should be 16
-
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // STARTTLS
-            auth: {
-                user: process.env.USER_EMAIL,
-                pass: process.env.APP_PW
-            },
-            tls: {
-                rejectUnauthorized: false, // bypass cert issues on Render
-            },
-        });
-        // Mail options
-        const mailOptions = {
-            from: `"MedTech" <${process.env.USER_EMAIL}>`,
+        await transporter.sendMail({
+            from: `"MedTech" <${process.env.EMAIL_FROM}>`,
             to: toEmail,
             subject,
             text,
             html,
-        };
-
-        // Send mail and await response
-     const info = await transporter.sendMail(mailOptions);
-
-    console.log("Email sent successfully:", info.messageId);
-    return info; // Return success info
+        })
+       
+       
     } catch (error) {
         console.error(MESSAGES.COMMON.EMAIL_ERROR, error);
-        throw error;
+       console.error(error);
     }
 };
 
