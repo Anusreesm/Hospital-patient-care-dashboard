76fb5038bfa4a44b1aa2123ed47ec4beadd6631e
ANUSREE S M <anusm43cal@gmail.com>
2025-12-04 11:51:52 +0530

    .

--- backend/utils/emails/sendMail.js
+++ backend/utils/emails/sendMail.js
@@ -12,8 +12,7 @@ const transporter=nodemailer.createTransport({
     auth:{
         user:process.env.USER_EMAIL,
         pass:process.env.USER_PASSWORD,
-    },
-    connectionTimeout: 20000,
+    }
     })
 
 const SendMail = async (toEmail, { subject, text, html }) => {
