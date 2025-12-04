b59458a71df0931971acf970d7e9d81386ab3867
ANUSREE S M <anusm43cal@gmail.com>
2025-12-04 19:22:48 +0530

    .

--- backend/utils/emails/sendMail.js
+++ backend/utils/emails/sendMail.js
@@ -1,32 +1,39 @@
-import nodemailer from 'nodemailer'
+import Mailjet from 'node-mailjet';
 import dotenv from "dotenv";
 
 import { MESSAGES } from "../../constants/messages.js";
 
 dotenv.config()
-
-const transporter=nodemailer.createTransport({
-    host:'smtp.gmail.com',
-    port:587,
-    secure: false,
-    auth:{
-        user:process.env.USER_EMAIL,
-        pass:process.env.USER_PASSWORD,
-    }
-    })
+const mailjet = Mailjet.apiConnect(
+    process.env.MAILJET_API_KEY,
+    process.env.MAILJET_SECRET_KEY
+);
 
 const SendMail = async (toEmail, { subject, text, html }) => {
     try {
-        console.log("useremail",process.env.USER_EMAIL)
-        console.log("from email",process.env.EMAIL_FROM)
-         transporter.sendMail({
-            from: process.env.EMAIL_FROM,
-            to: toEmail,
-            subject,
-            text,
-            html,
-        })
-       
+        const request = await mailjet
+            .post("send", { version: "v3.1" })
+            .request({
+                Messages: [
+                    {
+                        From: {
+                            Email: process.env.EMAIL_FROM.match(/<(.*)>/)[1],
+                            Name: process.env.EMAIL_FROM.split("<")[0].trim()
+                        },
+                        To: [
+                            {
+                                Email: toEmail
+                            }
+                        ],
+                        Subject: subject,
+                        TextPart: text,
+                        HTMLPart: html
+                    }
+                ]
+            });
+
+        console.log("Mailjet response:", request.body);
+        return request.body;
        
     } catch (error) {
         console.error(MESSAGES.COMMON.EMAIL_ERROR, error);
